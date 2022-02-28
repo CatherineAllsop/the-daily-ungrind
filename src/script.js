85e96283f2ecf1d2fad101cc30cbe7cec18220e9
Catherine <c.allsop32@gmail.com>
2022-02-28 17:58:18 +0000

    Site links CSS & footer

---
+++ src/script.js
@@ -0,0 +1,6 @@
+let subscribeForm = document.querySelector("#subscribe-form");
+function subscribeForm (event) {
+   event.preventDefault();
+   let subscribeFormInput=document.querySelector ("#email-input");
+}
+subscribeForm.addEventListener("submit", subscribeForm)
